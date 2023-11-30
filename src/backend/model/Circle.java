1489751a1b7778290580cd16f5b646456118829f
Borda Tomas <80111893+Bordita@users.noreply.github.com>
2023-11-30 19:20:20 -0300

    Update Circle.java

--- src/backend/model/Circle.java
+++ src/backend/model/Circle.java
@@ -1,6 +1,6 @@
 package backend.model;
 
-public class Circle extends Ellipse implements Figure {
+public class Circle extends Ellipse {
     public Circle(Point centerPoint, double radius) {
         super(centerPoint, radius * 2, radius * 2);
     }
