eeb23b05205932666bc8732ceacd31dc8f93e5ee
Borda Tomas <80111893+Bordita@users.noreply.github.com>
2023-11-30 19:20:31 -0300

    Update Square.java

--- src/backend/model/Square.java
+++ src/backend/model/Square.java
@@ -1,6 +1,6 @@
 package backend.model;
 
-public class Square extends Rectangle implements Figure {
+public class Square extends Rectangle{
     public Square(Point topLeft, double size) {
         super(topLeft, new Point(topLeft.getX() + size, topLeft.getY() + size));
     }
