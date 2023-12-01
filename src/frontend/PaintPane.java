437a818ee5b62f0581a64db8a890d6e8c439d644
Acuzito55 <asbsola@gmail.com>
2023-12-01 10:14:44 -0300

    fix: removed unnesessary imports

--- src/frontend/PaintPane.java
+++ src/frontend/PaintPane.java
@@ -14,9 +14,6 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class PaintPane extends BorderPane {
 	// BackEnd
 	CanvasState<DrawableFigure> canvasState;
