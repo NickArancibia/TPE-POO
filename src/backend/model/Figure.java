1d9e842dc1e2f768dd3eb0a748149b4527626d01
Nicolas Arancibia <152515634+NickArancibia@users.noreply.github.com>
2023-12-01 17:12:29 -0300

    add isFigureInRectangle method

--- src/backend/model/Figure.java
+++ src/backend/model/Figure.java
@@ -3,4 +3,5 @@ package backend.model;
 public interface Figure {
     boolean pointInFigure(Point p);
     void move(double deltaX, double deltaY);
+    boolean isFigureInRectangle(Point topLeft, Point bottomRight);
 }
